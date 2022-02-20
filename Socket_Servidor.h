6803e79151a6d3a8c301c19b9f00542d9bca9847
BrayanSR14 <99772706+BrayanSR14@users.noreply.github.com>
2022-02-20 16:21:58 -0600

    Update Socket_Servidor.h

--- Socket_Servidor.h
+++ Socket_Servidor.h
@@ -2,13 +2,10 @@
 #ifndef _SOCKET_SERVIDOR_H
 #define _SOCKET_SERVIDOR_H
 
-
 int Abre_Socket_Inet (char *Servicio);
 
 
-
 int Abre_Socket_Udp (char *Servicio);
-
 
 
 int Abre_Socket_Unix (char *Servicio);
