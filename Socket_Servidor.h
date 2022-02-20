91001cbc1a31b05288bed2ad54e41639b63b85f3
AngelVega26 <85775573+AngelVega26@users.noreply.github.com>
2022-02-19 19:40:52 -0600

    Create Socket_Servidor.h

---
+++ Socket_Servidor.h
@@ -0,0 +1,18 @@
+
+#ifndef _SOCKET_SERVIDOR_H
+#define _SOCKET_SERVIDOR_H
+
+
+int Abre_Socket_Inet (char *Servicio);
+
+
+
+int Abre_Socket_Udp (char *Servicio);
+
+
+
+int Abre_Socket_Unix (char *Servicio);
+
+int Acepta_Conexion_Cliente (int Descriptor);
+
+#endif
