81f31ec4694fdb42797389c08670c68690f43e20
BrayanSR14 <99772706+BrayanSR14@users.noreply.github.com>
2022-02-20 15:59:10 -0600

    Update SocketServidor.java

--- SocketServidor.java
+++ SocketServidor.java
@@ -23,16 +23,13 @@ public class SocketServidor
             
     
             cliente.setSoLinger (true, 10);
-
          
             DatoSocket dato = new DatoSocket("Hola, que tal");
-
         
             DataOutputStream bufferSalida = new DataOutputStream (cliente.getOutputStream());
             
 
             DataInputStream bufferEntrada = new DataInputStream (cliente.getInputStream());
-
          
             DatoSocket aux = new DatoSocket("");
             aux.readObject (bufferEntrada);
